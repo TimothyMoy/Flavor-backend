94169b6387f2cba68ca89f19a74f51a09c9844e0
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-14 16:53:17 -0700

    testing index route

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(4000, ()=> {
+  console.log('this is on port 4000')
+})
+
