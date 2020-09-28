1ea8bef6ecfd34f919784af256dd7ad5928ddce6
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-28 16:44:36 -0700

    created delete route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -10,14 +10,14 @@ const index = (req, res) => {
 
 const show = (req, res) => {
   db.Recipe.findById(req.params.id, (err, foundRecipes) => {
-    if (err) console.log('Error in recipe#show: ', err);
+    if (err) console.log('Error in recipe#show:', err);
     res.status(200).send(foundRecipes);
   });
 };
 
 const create = (req, res) => {
   db.Recipe.create(req.body, (err,savedRecipe) => {
-    if (err) console.log('Error in recipes#create', err);
+    if (err) console.log('Error in recipes#create:', err);
 
     res.status(200).json(savedRecipe);
   });
@@ -25,7 +25,7 @@ const create = (req, res) => {
 
 const update = (req, res) => {
   db.Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true }, (err, updatedRecipe) => {
-    if (err) console.log('Error in recipes#update', err);
+    if (err) console.log('Error in recipes#update:', err);
 
     if(!updatedRecipe) {
       res.status(400).json({message: `Could not find Recipe with id ${req.params.id}`});
@@ -35,9 +35,18 @@ const update = (req, res) => {
   });
 };
 
+const destory = (req, res) => {
+  db.Recipe.findByIdAndDelete(req.params.id, (err, deletedRecipe) => {
+    if (err) console.log('Error in recipes#destory:', err);
+
+    res.status(200).json(deletedRecipe);
+  });
+};
+
 module.exports = {
   index,
   show,
   create,
   update,
+  destory,
 };
